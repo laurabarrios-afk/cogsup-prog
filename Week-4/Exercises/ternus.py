76e49a6b3f889aef3d133246b415038b651432e1
first_name last_name <laura.barrios@ucp.edu.co>
2025-10-05 23:59:02 +0200

    Exercises week 4

--- Week-4/Exercises/ternus.py
+++ Week-4/Exercises/ternus.py
@@ -6,25 +6,38 @@ control.initialize(exp)
 width,height=exp.screen.size
 canvas=stimuli.Canvas(size=(width,height))
 
-posX1=[(-width/4)*3,-width/4,width/4] #Establishes positions for the first 3 circles and tags
-posX2=[-width/4,width/4,(width/4)*3] #Changes the position of the first circle and tag
+posX1=[(-width/8)*3,-width/8,width/8] #Establishes positions for the first 3 circles and tags
+posX2=[-width/8,width/8,(width/8)*3] #Changes the position of the first circle and tag
 colors1=[C_YELLOW,C_BLUE,C_RED] #Establishes colors for the first 3 tags
 colors2=[C_BLUE,C_RED,C_YELLOW] #"Moves" the first tag to the right 
+
+def load(stims):
+    for stim in stims:
+        stim.preload()
 
 def run_trial(rad,use_tags,isi):
     circles1 = [stimuli.Circle(radius=rad, position=(pos,0), colour=C_WHITE) for pos in posX1] #Creates the first 3 circles
     circles2 = [stimuli.Circle(radius=rad, position=(pos,0), colour=C_WHITE) for pos in posX2] #Moves the first circle to the right
     tags1 = [stimuli.Circle(radius=20, position=(pos,0), colour=color) for pos,color in zip(posX1,colors1)] #Changes the position of the first circle and tag
     tags2 = [stimuli.Circle(radius=20, position=(pos,0), colour=color) for pos,color in zip(posX2,colors2)] #Moves thefirst tag to the right 
+
+    load(circles1,circles2,tags1,tags2)
+
     while True:
         if use_tags: #If use_tags=True, stims becomes a list of circles+tags, with tags last so that the can be drawed on top of the circles
             present_for(circles1+tags1,12) #Displays all the items of the list and waits for 200 ms
-            exp.clock.wait(isi*16.67) #Waits the given ISI
+            if isi>0:
+                exp.clock.wait(isi*16.67) #Waits the given ISI
             present_for(circles2+tags2,12) #Displays all the items with the new positions 
+            if isi>0:
+                exp.clock.wait(isi*16.67)
         else: #Displays only the big circles
             present_for(circles1,12) 
-            exp.clock.wait(isi*16.67)
+            if isi>0:
+                exp.clock.wait(isi*16.67)
             present_for(circles2,12)
+            if isi>0:
+                exp.clock.wait(isi*16.67)
         if exp.keyboard.check(K_SPACE): #Allows to exit the loop
             break
         
@@ -39,6 +52,6 @@ def present_for(stims,frames):
         
 run_trial(rad=100, isi=3, use_tags=False)
 run_trial(rad=100, isi=30, use_tags=False)
-run_trial(rad=100, isi=3, use_tags=True)
+run_trial(rad=100, isi=30, use_tags=True)
 
 control.end()
